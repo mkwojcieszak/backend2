1de4540093cfc22eac31022590a9eb381b986758
Kirin <mkwojcieszak@gmail.com>
2020-07-13 20:12:12 +0200

    Added date picker and some conveniences to likes module

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -6,6 +6,7 @@ use App\Entity\Product;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class ProductType extends AbstractType
@@ -15,7 +16,9 @@ class ProductType extends AbstractType
         $builder
             ->add('name')
             ->add('info')
-            ->add('public_date')
+            ->add('public_date', DateType::class, [
+                'widget' => 'single_text'
+            ])
             ->add('save', SubmitType::class, [
                 'attr' => [ 'class' => 'btn btn-success']
             ])
